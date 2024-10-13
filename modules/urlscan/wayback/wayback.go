628b3264ce4c57fdf61adcc954133624c39db578
Autumn.home <autumn@home.com>
2024-10-14 00:00:21 +0800

    wayback

---
+++ modules/urlscan/wayback/wayback.go
@@ -0,0 +1,8 @@
+// wayback-------------------------------------
+// @file      : wayback.go
+// @author    : Autumn
+// @contact   : rainy-autumn@outlook.com
+// @time      : 2024/10/13 23:43
+// -------------------------------------------
+
+package wayback
