e82bb64264461d6ddd92891446689715fd134d17
Autumn.home <autumn@home.com>
2024-10-20 16:29:26 +0800

    fix bug

--- internal/task/task.go
+++ internal/task/task.go
@@ -9,6 +9,7 @@ package task
 
 import (
 	"fmt"
+	"github.com/Autumn-27/ScopeSentry-Scan/internal/handle"
 	"github.com/Autumn-27/ScopeSentry-Scan/internal/options"
 	"github.com/Autumn-27/ScopeSentry-Scan/internal/pebbledb"
 	"github.com/Autumn-27/ScopeSentry-Scan/internal/pool"
@@ -27,66 +28,72 @@ func GetTask() {
 		logger.SlogErrorLocal(fmt.Sprintf("pebbledb get task error: %v", err))
 		os.Exit(0)
 	}
-	// 打印所有以 "task:" 开头的键值对
-	for key, value := range keys {
-		var wg sync.WaitGroup
-		logger.SlogInfoLocal(fmt.Sprintf("get PebbleStore task: %v", string(value)))
-		var runnerOption options.TaskOptions
-		err = utils.Tools.JSONToStruct(value, &runnerOption)
-		if err != nil {
-			logger.SlogErrorLocal(fmt.Sprintf("task JSONToStruct error: %v - %v", string(value), err))
-			err = pebbledb.PebbleStore.Delete([]byte(fmt.Sprintf("task:%s", value)))
+	if len(keys) > 0 {
+		handle.NewNucleiEngine()
+		// 打印所有以 "task:" 开头的键值对
+		for key, value := range keys {
+			var wg sync.WaitGroup
+			logger.SlogInfoLocal(fmt.Sprintf("get PebbleStore task: %v", string(value)))
+			var runnerOption options.TaskOptions
+			err = utils.Tools.JSONToStruct(value, &runnerOption)
 			if err != nil {
-				logger.SlogErrorLocal(fmt.Sprintf("PebbleStore delete error: %v", value))
-			}
-			continue
-		}
-		prefix = fmt.Sprintf("%s:", runnerOption.ID)
-		targets, err := pebbledb.PebbleStore.GetKeysWithPrefix(prefix)
-		if err != nil {
-			logger.SlogErrorLocal(fmt.Sprintf("pebbledb get task targets error: %v", err))
-			err = pebbledb.PebbleStore.Delete([]byte(fmt.Sprintf("task:%s", value)))
-			if err != nil {
-				logger.SlogErrorLocal(fmt.Sprintf("PebbleStore delete error: %v", value))
-			}
-			continue
-		}
-		for idTarget, _ := range targets {
-			wg.Add(1)
-			// 创建 runnerOption 的副本
-			optionCopy := runnerOption
-			target := strings.SplitN(idTarget, ":", 2)
-			optionCopy.Target = target[1]
-			// 使用局部变量创建闭包
-			taskFunc := func(op options.TaskOptions) func() {
-				return func() {
-					defer func(PebbleStore *pebbledb.PebbleDB, targetKey []byte) {
-						err := PebbleStore.Delete(targetKey)
-						if err != nil {
-							logger.SlogErrorLocal(fmt.Sprintf("PebbleStore Delete error: %v", err))
-						}
-					}(pebbledb.PebbleStore, []byte(op.ID+":"+op.Target))
-					defer wg.Done()
-					runner.Run(op)
+				logger.SlogErrorLocal(fmt.Sprintf("task JSONToStruct error: %v - %v", string(value), err))
+				err = pebbledb.PebbleStore.Delete([]byte(fmt.Sprintf("task:%s", value)))
+				if err != nil {
+					logger.SlogErrorLocal(fmt.Sprintf("PebbleStore delete error: %v", value))
 				}
-			}(optionCopy)
-
-			// 提交任务
-			err := pool.PoolManage.SubmitTask("task", taskFunc)
-			if err != nil {
-				logger.SlogError(fmt.Sprintf("task pool error: %v", err))
-				// 如果提交任务失败，手动减少计数
-				wg.Done()
+				continue
 			}
+			prefix = fmt.Sprintf("%s:", runnerOption.ID)
+			targets, err := pebbledb.PebbleStore.GetKeysWithPrefix(prefix)
+			if err != nil {
+				logger.SlogErrorLocal(fmt.Sprintf("pebbledb get task targets error: %v", err))
+				err = pebbledb.PebbleStore.Delete([]byte(fmt.Sprintf("task:%s", value)))
+				if err != nil {
+					logger.SlogErrorLocal(fmt.Sprintf("PebbleStore delete error: %v", value))
+				}
+				continue
+			}
+			for idTarget, _ := range targets {
+				wg.Add(1)
+				// 创建 runnerOption 的副本
+				optionCopy := runnerOption
+				target := strings.SplitN(idTarget, ":", 2)
+				optionCopy.Target = target[1]
+				// 使用局部变量创建闭包
+				taskFunc := func(op options.TaskOptions) func() {
+					return func() {
+						defer func(PebbleStore *pebbledb.PebbleDB, targetKey []byte) {
+							err := PebbleStore.Delete(targetKey)
+							if err != nil {
+								logger.SlogErrorLocal(fmt.Sprintf("PebbleStore Delete error: %v", err))
+							}
+						}(pebbledb.PebbleStore, []byte(op.ID+":"+op.Target))
+						defer wg.Done()
+						runner.Run(op)
+					}
+				}(optionCopy)
+
+				// 提交任务
+				err := pool.PoolManage.SubmitTask("task", taskFunc)
+				if err != nil {
+					logger.SlogError(fmt.Sprintf("task pool error: %v", err))
+					// 如果提交任务失败，手动减少计数
+					wg.Done()
+				}
+			}
+			wg.Wait()
+			err = pebbledb.PebbleStore.Delete([]byte("task:" + key))
+			if err != nil {
+				logger.SlogErrorLocal(fmt.Sprintf("PebbleStore Delete %v error: %v", key, err))
+			}
+			// 记得判断是否需要增加一个等待 所有目标执行完毕再任务结束
+			fmt.Printf("任务结束: %v\n", runnerOption.ID)
 		}
-		wg.Wait()
-		err = pebbledb.PebbleStore.Delete([]byte("task:" + key))
-		if err != nil {
-			logger.SlogErrorLocal(fmt.Sprintf("PebbleStore Delete %v error: %v", key, err))
-		}
-		// 记得判断是否需要增加一个等待 所有目标执行完毕再任务结束
-		fmt.Printf("任务结束: %v\n", runnerOption.ID)
+
+		handle.CloseNucleiEngine()
 	}
+
 }
 
 func GetPebbledbTaskTarget(id string) {
