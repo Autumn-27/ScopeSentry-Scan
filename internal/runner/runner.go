31eac928f63a1f51fb9d50f680419ffe4250bd70
Autumn.home <autumn@home.com>
2024-12-05 22:41:03 +0800

    fix delete /tmp

--- internal/runner/runner.go
+++ internal/runner/runner.go
@@ -13,11 +13,17 @@ import (
 	"github.com/Autumn-27/ScopeSentry-Scan/internal/handler"
 	"github.com/Autumn-27/ScopeSentry-Scan/internal/options"
 	"github.com/Autumn-27/ScopeSentry-Scan/modules"
+	"github.com/Autumn-27/ScopeSentry-Scan/pkg/logger"
+	"os/exec"
+	"os/user"
+	"runtime"
 	"sync"
 	"time"
 )
 
 func Run(op options.TaskOptions) error {
+	// 清空临时文件
+	defer CleanTmp()
 	var wg sync.WaitGroup
 	var start time.Time
 	var end time.Time
@@ -55,5 +61,69 @@ func Run(op options.TaskOptions) error {
 		// 增加完成计数
 		handler.TaskHandle.EndTask()
 		return nil
+	}
+}
+
+func CleanTmp() {
+	osType := runtime.GOOS
+	fmt.Printf("当前操作系统: %s\n", osType)
+
+	if osType == "windows" {
+		// Windows 系统处理
+		handleWindowsTemp()
+	} else if osType == "linux" {
+		// Linux 系统处理
+		handleLinuxTemp()
+	}
+}
+
+func handleWindowsTemp() {
+	// 获取当前用户名
+	currentUser, err := user.Current()
+	if err != nil {
+		return
+	}
+	tempDir := fmt.Sprintf(`C:\Users\%s\AppData\Local\Temp`, currentUser.Username)
+
+	// 定义 PowerShell 命令
+	psCmd := fmt.Sprintf(`
+Get-ChildItem -Path "%s" -Directory |
+Where-Object {
+    ($_.Name -match '^\d{9}$') -or 
+    ($_.Name -like 'ScopeSentry*') -or 
+    ($_.Name -like '.org.chromium.Chromium*') -or 
+    ($_.Name -like '*_badger') -or
+	($_.Name -like 'nuclei*') -or
+	($_.Name -like 'rod*')
+} |
+Remove-Item -Recurse -Force
+`, tempDir)
+
+	// 执行 PowerShell 命令
+	cmd := exec.Command("powershell", "-Command", psCmd)
+	_, err = cmd.CombinedOutput()
+	if err != nil {
+		fmt.Printf("执行 PowerShell 命令时出错: %v\n", err)
+		logger.SlogWarn("清空临时文件C:\\Users\\{username}\\AppData\\Local\\Temp\\[^\\d{9}$、ScopeSentry*、.org.chromium.Chromium*、*_badger]失败，请手动清空，防止占用磁盘过大，")
+		return
+	}
+}
+
+func handleLinuxTemp() {
+	tempDir := "/tmp"
+
+	// 定义 Linux 下的 find 命令
+	findCmd := fmt.Sprintf(
+		`find %s -type d \( -regex '.*/[0-9]\{9\}' -o -name 'ScopeSentry*' -o -name 'nuclei*' -o -name '.org.chromium.Chromium*' -o -name '*_badger' -o -name 'rod' \) -exec rm -rf {} +`,
+		tempDir,
+	)
+
+	// 执行命令
+	cmd := exec.Command("bash", "-c", findCmd)
+	_, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Printf("执行 Linux find 命令时出错: %v\n", err)
+		logger.SlogWarn("清空临时文件出错，请手动清空/tmp目录，防止磁盘占用过大")
+		return
 	}
 }
