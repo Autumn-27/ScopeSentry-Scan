3cff5dc42cddd1c279f904fbd199b6eb6a8b9dde
Autumn.home <autumn@home.com>
2025-02-08 19:30:55 +0800

    add module passive scan

---
+++ modules/passivescan/module.go
@@ -0,0 +1,8 @@
+// passivescan-------------------------------------
+// @file      : module.go
+// @author    : Autumn
+// @contact   : rainy-autumn@outlook.com
+// @time      : 2025/2/8 19:30
+// -------------------------------------------
+
+package passivescan
